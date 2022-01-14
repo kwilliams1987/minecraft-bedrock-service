8d85f7e90f31e31eb8cd9e96966fc9836b9100b9
Kristian Williams <kristian.andersonwilliams@gmail.com>
2022-01-14 09:06:47 +0100

    - Minor style fix.

--- MinecraftBedrockService/ServerManager.cs
+++ MinecraftBedrockService/ServerManager.cs
@@ -83,6 +83,7 @@ namespace MinecraftBedrockService
                 serverProcess = null;
                 return Task.FromResult(false);
             }
+
             if (serverExecutable.Exists)
             {
                 try
